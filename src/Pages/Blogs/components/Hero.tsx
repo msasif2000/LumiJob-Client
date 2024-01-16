55dcffca336fffee31a2db6e5a4ded5ee1e68286
nasimrifat101 <nasimrifat101@gmail.com>
2024-01-17 00:28:03 +0600

    did some tweaking

---
+++ src/Pages/Blogs/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div className="min-h-screen bg-[#F2F8F5]">
+      <div className="max-w-screen-2xl mx-auto">
+       <div>
+         <h1 className="text-[160px] font-bold pt-20">News, Seminars & Insides of Tech Fields.</h1>
+       </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
