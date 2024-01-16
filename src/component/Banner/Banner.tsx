8c23151e94b352e6d1f06a286b8e6e050ae109ef
mehedihasanepu <mehedihasanepu123@gmail.com>
2024-01-17 01:38:18 +0600

    complete footer section

--- src/component/Banner/Banner.tsx
+++ src/component/Banner/Banner.tsx
@@ -29,7 +29,7 @@ const Banner = () => {
                                 <img className="absolute z-2 bg-white w-10 md:w-12 lg:w-16 top-16 lg:top-24 left-16 lg:left-32 rounded-lg drop-shadow-xl p-3" src={googleIcon} alt="" />
                                 <img className="absolute z-2 bg-white w-10 md:w-12 lg:w-16 top-40 md:top-48 lg:top-60 left-8 lg:left-20 rounded-lg drop-shadow-xl p-3" src={dropboxIcon} alt="" />
                             </div>
-                            <div className="absolute bottom-0 lg:bottom-28 right-1 bg-white rounded-xl drop-shadow-xl p-4 w-[205px] md:w-[260px] lg:w-[300px]">
+                            <div className="absolute bottom-0 lg:bottom-20 right-1 bg-white rounded-xl drop-shadow-xl p-4 w-[205px] md:w-[260px] lg:w-[300px]">
                                 <div className="flex items-center gap-2 ">
                                     <img className="w-8 lg:w-12" src={twitterIcon} alt="" />
                                     <div className="flex flex-col gap-1">
@@ -37,11 +37,11 @@ const Banner = () => {
                                         <p className="text-xs lg:text-base text-gray-600">California, USA</p>
                                     </div>
                                 </div>
-                                <p className="md:text-xs lg:text-base text-gray-600 pt-2 hidden md:flex">Online social networking and <span className="md:hidden lg:flex ">micro-blogging service that</span>...<NavLink className="text-blue-700 md:text-xs lg:text-base font-semibold" to="/">Read more</NavLink> </p>
-                                <div className="pt-3">
-                                    <button className="btn btn-sm bg-blue-100 p-[5px] md:p-3 text-[11px] md:text-[12px] lg:text-sm text-blue-900 normal-case"><NavLink className="flex items-center gap-2" to="/">Fulltime</NavLink></button>
-                                    <button className="btn btn-sm bg-green-10 p-[5px] md:p-3 text-[11px] md:text-[12px] lg:text-sm text-green-800 normal-case mx-1 lg:mx-3"><NavLink className="flex items-center gap-2" to="/">Remote</NavLink></button>
-                                    <button className="btn btn-sm bg-fuchsia-100 p-[5px] md:p-3 text-[11px] md:text-[12px] lg:text-sm text-fuchsia-800 normal-case"><NavLink className="flex items-center gap-2" to="/">Anywhere</NavLink></button>
+                                <p className="md:text-xs lg:text-base text-gray-600 pt-2 hidden md:flex md:flex-col">Online social networking and micro-blogging service that...<NavLink className="text-blue-700 text-xs lg:text-base font-semibold" to="/">Read more</NavLink> </p>
+                                <div className="mt-3">
+                                    <button className="btn btn-sm bg-blue-100 p-[5px] md:p-3 lg:p-3 text-[11px] md:text-[12px]  text-blue-900 normal-case">Fulltime</button>
+                                    <button className="btn btn-sm bg-green-10 p-[5px] md:p-3 lg:p-3 text-[11px] md:text-[12px] text-green-800 normal-case mx-1 lg:mx-3">Remote</button>
+                                    <button className="btn btn-sm bg-fuchsia-100 p-[5px] md:p-3 lg:p-3 text-[11px] md:text-[12px] text-fuchsia-800 normal-case">Anywhere</button>
                                 </div>
                             </div>
                         </div>
