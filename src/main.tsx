9917f62eb5f1b4790366d1a44cafb722d1f87ab8
Sejan~ <md4sejan@gmail.com>
2024-01-16 15:05:46 +0600

    Theme provider and context

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import Main from './Layout/Main';
-import Home from './Pages/Home/Home';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Main from "./Layout/Main";
+import Home from "./Pages/Home/Home";
+import ThemeProvider from "./providers/ThemeProvider";
 
 const router = createBrowserRouter([
   {
@@ -15,15 +13,17 @@ const router = createBrowserRouter([
     element: <Main></Main>,
     children: [
       {
-        path: '/',
-        element: <Home></Home>
-      }
-    ]
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
