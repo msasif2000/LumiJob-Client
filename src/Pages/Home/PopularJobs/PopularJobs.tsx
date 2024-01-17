12464ca5dc13ef8132e0e653963e2e38ed823b9f
Sejan~ <md4sejan@gmail.com>
2024-01-17 12:42:42 +0600

    open sans font  added

--- src/Pages/Home/PopularJobs/PopularJobs.tsx
+++ src/Pages/Home/PopularJobs/PopularJobs.tsx
@@ -1,34 +1,32 @@
 import { useEffect, useState } from "react";
 import JobCards from "./JobCards";
 
-
 const PopularJobs = () => {
-    
-    const [popularJobs, setPopularJobs] = useState([]);
+  const [popularJobs, setPopularJobs] = useState([]);
 
-    useEffect(() => {
-        fetch('popular.json')
-            .then(res => res.json())
-            .then(data => setPopularJobs(data))
-    }, [])
+  useEffect(() => {
+    fetch("popular.json")
+      .then((res) => res.json())
+      .then((data) => setPopularJobs(data));
+  }, []);
 
-    return (
-        <>
+  return (
+    <>
       <div className="">
-
-        <h3 className="text-4xl font-bold text-center mb-5">Most popular jobs for you</h3>
-        <p className="text-sm text-center mb-10">The most updated platform about jobs that are open</p>
+        <h3 className="text-4xl font-bold text-center mb-5">
+          Most popular jobs for you
+        </h3>
+        <p className="text-sm text-center mb-10">
+          The most updated platform about jobs that are open
+        </p>
         <div className="grid md:grid-cols-4 gap-5">
-          {
-            popularJobs.map(job => <JobCards key={job.id}
-              job={job}
-            >
-            </JobCards>)
-          }
+          {popularJobs.map((job) => (
+            <JobCards key={job.id} job={job}></JobCards>
+          ))}
         </div>
       </div>
     </>
-    );
+  );
 };
 
 export default PopularJobs;
