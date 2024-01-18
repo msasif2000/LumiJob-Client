79b52d85c35586574ed2fbcc2a143ca7fada7bf3
nasimrifat101 <nasimrifat101@gmail.com>
2024-01-18 18:17:10 +0600

    login page made responsive

--- src/Pages/Auth/Login.tsx
+++ src/Pages/Auth/Login.tsx
@@ -37,14 +37,14 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="w-full h-screen flex">
+    <div className="w-full h-screen flex px-3 md:px-10 lg:px-0">
       {/* form div */}
-      <div className="w-1/2 flex items-center justify-center">
-        <div className="px-40  space-y-10">
-          <h1 className="text-5xl font-semibold">
-            Welcome Back <span className="text-5xl">👋🏻</span>
+      <div className="lg:w-1/2 flex items-center justify-center">
+        <div className="space-y-5 lg:px-5 xl:px-40  md:space-y-10">
+          <h1 className="text-2xl md:text-4xl xl:text-5xl font-semibold">
+            Welcome Back <span className="lg:text-5xl">👋🏻</span>
           </h1>
-          <p className="text-2xl text-gray-500 font-medium ">
+          <p className="md:text-xl xl:text-2xl text-gray-500 font-medium ">
             Today is a new day. It's your day. You shape it. Sign in to start
             managing your Career path.
           </p>
@@ -58,7 +58,7 @@ const Login: React.FC = () => {
                 type="email"
                 {...register("email", { required: "Email is required" })}
                 placeholder="Example@gmail.com"
-                className="input-lg rounded-lg border-b-4 hover:border-b-teal-500 duration-500 outline-none bg-[#F7FBFF]"
+                className="input-md md:input-lg rounded-lg border-b-4 hover:border-b-teal-500 duration-500 outline-none bg-[#F7FBFF]"
               />
             </div>
             <div className="form-control w-full">
@@ -77,7 +77,7 @@ const Login: React.FC = () => {
                   },
                 })}
                 placeholder="at least 6 character"
-                className="input-lg border-b-4 outline-none rounded-lg  bg-[#F7FBFF] hover:border-b-teal-500 duration-500"
+                className="input-md md:input-lg border-b-4 outline-none rounded-lg  bg-[#F7FBFF] hover:border-b-teal-500 duration-500"
               />
               {errors.password && (
                 <p className="text-red-500">{errors.password.message}</p>
@@ -91,14 +91,14 @@ const Login: React.FC = () => {
             <input
               type="submit"
               value="Sign in"
-              className="btn btn-lg w-full bg-[#162D3A] text-white hover:bg-green-400 hover:text-black duration-500 "
+              className="btn md:btn-lg w-full bg-[#162D3A] text-white hover:bg-green-400 hover:text-black duration-500 "
             />
           </form>
           <div className="divider divider-neutral">Or</div>
           {/* Social Login Section */}
           <SocialLogin />
 
-          <div className="flex justify-between text-xl">
+          <div className="flex justify-between lg:text-xl">
             <p>Don't have an account?</p>
             <Link to="/signup">
               <p className="hover:text-green-500 cursor-pointer text-violet-400 font-semibold underline">
@@ -110,7 +110,7 @@ const Login: React.FC = () => {
       </div>
 
       {/* Image Div */}
-      <div className="w-1/2 object-cover m-5 rounded ">
+      <div className="lg:w-1/2 object-cover m-5 rounded hidden lg:block">
         <img
           src={LoginArt}
           className="w-full h-full object-cover rounded-xl"
