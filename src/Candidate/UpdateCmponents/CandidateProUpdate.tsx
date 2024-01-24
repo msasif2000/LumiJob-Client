5879e6342db76629bd8a4a1c10e375d2c2a90299
nasimrifat101 <nasimrifat101@gmail.com>
2024-01-24 19:05:46 +0600

    little modification in update page

--- src/Candidate/UpdateCmponents/CandidateProUpdate.tsx
+++ src/Candidate/UpdateCmponents/CandidateProUpdate.tsx
@@ -46,14 +46,14 @@ const CandidateProUpdate: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="bg-base-200 min-h-screen p-5">
       <CandidateNav
         text="Upgrade your information"
         btn="Return"
         handleClick={backToProfile}
       />
       {/* Your navigation component */}
-      <div className="p-5">
+      <div className="p-5 bg-white">
         <form onSubmit={handleSubmit(onSubmit)}>
           {/* Name */}
           <div className="form-control w-full">
