c8af7df0d2c28875aa632bde3e08ff5ce456fd92
mehedihasanepu <mehedihasanepu123@gmail.com>
2024-01-25 00:01:41 +0600

    feat(feature-companyProfile):company profile info section complete

--- src/Company/CompanyProfile.tsx
+++ src/Company/CompanyProfile.tsx
@@ -1,8 +1,49 @@
+import { MdOutlineModeEdit } from "react-icons/md";
 
 
 const CompanyProfile = () => {
     return (
         <div>
+            <div className="h-60 mt-10 px-7 bg-gradient-to-r from-[#228bedde] to-[#6565C7]">
+                <div className="flex justify-between  my-2 pt-2">
+                    <div>
+                        <p className="text-2xl p-2 text-white ">Company Profile</p>
+                    </div>
+                    <div className="flex justify-center items-center bg-white rounded-3xl px-2 py-1">
+                        <div className="bg-[#6f6aff] p-1 rounded-full">
+                            <MdOutlineModeEdit className="text-2xl text-white" />
+                        </div>
+                        <p className="text-xl font-semibold text-[#6f6aff] p-2  ">Edit Profile</p>
+                    </div>
+                </div>
+                <div className="flex gap-5">
+                    <img className="w-28 h-28 rounded-full bg-white p-2" src="https://i.ibb.co/5cBJy9v/Tesla-logo.png" />
+                    <div >
+                        <p className="uppercase text-white ">Company Name </p>
+                        <p className="text-2xl font-bold text-white ">Tesla</p>
+                        <p className="uppercase text-white mt-5">Industry</p>
+                        <p className="text-xl font-semibold text-white ">Electric vehicle</p>
+                    </div>
+                    <div >
+                        <p className="uppercase text-white ">Location</p>
+                        <p className="text-lg font-semibold text-white ">Corporate Secretary
+                            Tesla, Inc.
+                            1 Tesla Road <br />
+                            Austin, TX 78725
+                            United States</p>
+                        <p className="uppercase text-white pt-1">Phone: +1-888-518-3752</p>
+                        <p className=" text-white pt-1">E-mail: support@tesla.com</p>
+
+                    </div>
+                    <div >
+                        <p className="uppercase text-white ">Website</p>
+                        <p className="text-lg font-semibold text-white ">www.tesla.com</p>
+                        <p className="uppercase text-white font-semibold pt-1">Founded: 2000</p>
+
+                    </div>
+                </div>
+            </div>
+
             
         </div>
     );
--- src/Layout/DashBoard.tsx
+++ src/Layout/DashBoard.tsx
@@ -16,13 +16,13 @@ const DashBoard = () => {
     }
     return (
         <div className="md:flex">
-            <div className="md:w-2/5 lg:w-1/4 xxl-w-1/6 md:flex-shrink-0">
+            <div className="md:w-2/5 lg:w-1/6 xxl-w-1/6 md:flex-shrink-0">
                 <div className="navbar-start md:hidden">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="red"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="sty menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-blue-600 rounded-box w-52 ">
+                        <ul tabIndex={0} className=" menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-blue-600 rounded-box w-52 ">
                             {
                                 role === 'candidate' ?
                                 <>
@@ -84,7 +84,7 @@ const DashBoard = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="md:flex hidden w-68 min-h-screen bg-blue-600 text-white pt-12">
+                <div className="md:flex hidden  min-h-screen bg-gray-200 pt-12">
 
                     <ul className="menu text-xl">
                         {
