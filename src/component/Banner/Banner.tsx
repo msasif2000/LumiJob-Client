c0a383a4d10bac405c3b77c01d4f8318092e2f81
Sejan~ <md4sejan@gmail.com>
2024-01-31 03:28:58 +0600

    [Fix/UI]: HowItWorks section typography and responsive

--- src/component/Banner/Banner.tsx
+++ src/component/Banner/Banner.tsx
@@ -13,12 +13,12 @@ const Banner = () => {
     >
       <div className="max-w-screen-xl mx-auto px-4 py-10">
         <div className="flex flex-col justify-center items-center text-center min-h-[60vh] md:min-h-[40vh] lg:min-h-[90vh] 2xl:min-h-[80vh]">
-          <h2 className="text-4xl md:text-6xl lg:text-7xl font-heading font-semibold my-12 relative z-10">
+          <h2 className="text-4xl md:text-6xl lg:text-7xl font-heading font-semibold my-12">
             <span className="text-accent">Empowering </span> Tech Careers with
             Innovation and
             <span className="text-accent"> Boundless </span> Opportunities
           </h2>
-          <p className=" text-sm md:text-base lg:text-lg xl:text-xl text-gray-600 px-5 md:px-28 lg:px-40 font-normal relative z-10">
+          <p className="text-md md:text-lg xl:text-xl text-gray-600 px-5 md:px-28 lg:px-40 font-normal">
             Unlock the Door to Your Future: Let LumiJobs Empower Your Tech
             Career Journey with Continuous Innovation, Abundant Opportunities,
             and Limitless Possibilities
