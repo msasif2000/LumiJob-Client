3e3c0735892085a8bdf59618283f6035561cbc78
Sejan~ <md4sejan@gmail.com>
2024-01-30 16:38:48 +0600

    feat(filterJob): category filter ui added

--- src/Pages/FindJob/Filters.tsx
+++ src/Pages/FindJob/Filters.tsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+import Sector from "../Home/PopularJobs/sector";
+
 const Filters = () => {
+  const [sectors, setSectors] = useState<Sector[]>([]);
+
+  useEffect(() => {
+    fetch("sectors.json")
+      .then((res) => res.json())
+      .then((data: Sector[]) => setSectors(data));
+  }, []);
+
   return (
     <div>
       <div className="col-span-1">
@@ -6,37 +17,55 @@ const Filters = () => {
           <h4 className="font-semibold font-hanken text-2xl">Filters</h4>
         </div>
         <div className="border rounded p-5 lg:p-8 m-3 min-h-[35vh] bg-white sticky top-24">
-          <h6 className="mb-3 text-sm font-medium text-gray-900">Category</h6>
-          <ul className="space-y-2 text-sm">
-            <li className="flex items-center">
-              <input
-                id="apple"
-                type="checkbox"
-                defaultValue=""
-                className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                htmlFor="apple"
-                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
-              >
-                Apple (56)
-              </label>
-            </li>
-            <li className="flex items-center">
-              <input
-                id="fitbit"
-                type="checkbox"
-                defaultValue=""
-                className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                htmlFor="fitbit"
-                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
-              >
-                Fitbit (56)
-              </label>
-            </li>
-          </ul>
+          {/* Category Filter */}
+          <div>
+            <h6 className="mb-3 text-xl font-hanken font-semibold text-gray-900">
+              Category
+            </h6>
+            <ul className="space-y-2 text-sm">
+              {sectors?.map((sector) => (
+                <li key={sector._id} className="flex items-center">
+                  <input
+                    type="checkbox"
+                    value={sector.sectorType}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+                  />
+                  <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    {sector.sectorType}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Job Type Filter */}
+          <div className="mb-5 mt-12">
+            <h6 className="mb-3 text-xl font-hanken font-semibold text-gray-900">
+              Job Type
+            </h6>
+            <ul className="space-y-2 text-sm">
+              <li className="flex items-center">
+                <input
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+                />
+                <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Full Time
+                </label>
+              </li>
+              <li className="flex items-center">
+                <input
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+                />
+                <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Remote
+                </label>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
