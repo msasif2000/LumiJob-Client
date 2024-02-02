d51e8b6041a1921efa041370c50e849772233ab5
Asif <msasifiiuc@gmail.com>
2024-02-02 18:38:29 +0600

    filtered functions are still in error phase

--- src/Layout/DashBoard.tsx
+++ src/Layout/DashBoard.tsx
@@ -8,7 +8,7 @@ import UniLoader from "../component/err & loading/UniLoader";
 const DashBoard = () => {
   const { user, logOut } = useAuth();
   const navigate = useNavigate();
-  // const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true);
 
   const [isNavOpen, setIsNavOpen] = useState(false);
   useEffect(() => {
@@ -23,22 +23,22 @@ const DashBoard = () => {
     };
   }, [isNavOpen]);
 
-  // const [role, setRole] = useState('');
-  // const axiosDev = useAxiosDev();
-  // useEffect(() => {
-  // setLoading(true);
-  // axiosDev.get(`/check-which-role/${user?.email}`).then((res) => {
-  //   console.log(res.data.role);
-  //   setRole(res.data.role);
-  //   setLoading(false);
-  // })
-  // .catch(error=>{
-  //   console.log(error);
-  //   setLoading(false);
-  // });
-  // }, [user]);
+  const [role, setRole] = useState('');
+  const axiosDev = useAxiosDev();
+  useEffect(() => {
+    setLoading(true);
+    axiosDev.get(`/check-which-role/${user?.email}`).then((res) => {
+      console.log(res.data.role);
+      setRole(res.data.role);
+      setLoading(false);
+    })
+      .catch(error => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, [user]);
 
-  const role: string = "candidate";
+  //const role: string = "company";
   const handleLogout = () => {
     logOut();
     navigate("/");
@@ -46,7 +46,196 @@ const DashBoard = () => {
 
   return (
     <>
-      <div className="md:flex">
+      {isLoading ? (
+        <UniLoader />
+      ) : (
+        <div className="md:flex">
+          <div className="lg:w-2/12 xxl-w-1/6 md:flex-shrink-0">
+            <div className="navbar-start lg:hidden">
+              <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="red"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />
+                  </svg>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className=" menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-blue-600 rounded-box  w-52 "
+                >
+                  {role === "candidate" ? (
+                    <>
+                      <li>
+                        <h2 className="text-xl xl:text-2xl text-red-800 font-bold bg-white my-2">
+                          Candidate DashBoard
+                        </h2>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/profile">Profile</NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/resume">Resume</NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/appliedJobs">
+                          Applied Jobs
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/bookmarks">Bookmarks</NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/analytics">Analytics</NavLink>
+                      </li>
+                    </>
+                  ) : (
+                    <>
+                      <li>
+                        <h2 className="text-2xl text-blue-800 font-bold bg-white my-2">
+                          Company DashBoard
+                        </h2>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/companyProfile">
+                          Profile
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/postJob">Post Job</NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/postedJobs">Posted Job</NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/companyAnalytics">
+                          Analytics
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/dashboard/employees">Employees</NavLink>
+                      </li>
+                    </>
+                  )}
+                  <div className="divider"></div>
+
+                  <li>
+                    <NavLink to="/">Home</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/availableCamp">Available Camp</NavLink>
+                  </li>
+                  <li>
+                    <button
+                      onClick={handleLogout}
+                      className="flex items-center btn btn-sm"
+                    >
+                      <BiLogOut className="text-2xl" />
+                      <span>Sign Out</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="lg:flex hidden  min-h-screen bg-gray-200 pt-12 lg:sticky  lg:top-0 lg:inset-x-0 lg:z-20">
+              <ul className="menu text-xl">
+                {role === "candidate" ? (
+                  <>
+                    <li>
+                      <h2 className="text-2xl text-blue-800 font-bold bg-white my-2">
+                        Candidate DashBoard
+                      </h2>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/candidateProfile">
+                        Profile
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/resume">Resume</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/appliedJobs">
+                        Applied Jobs
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/bookmarks">Bookmarks</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/candidateAnalytics">
+                        Analytics
+                      </NavLink>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <h2 className="text-2xl text-red-800 font-bold bg-white my-2">
+                        Company DashBoard
+                      </h2>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/companyProfile">Profile</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/postJob">Post Job</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/postedJobs">Posted Job</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/companyAnalytics">
+                        Analytics
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/employees">Employees</NavLink>
+                    </li>
+                  </>
+                )}
+
+                <div className="divider"></div>
+
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                  <button onClick={handleLogout} className="flex items-center">
+                    <BiLogOut className="text-2xl" />
+                    <span>Sign Out</span>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="md:flex-1 overflow-x-auto px-5">
+            <Outlet></Outlet>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default DashBoard;
+
+
+
+
+
+
+
+{/* <div className="md:flex">
         <div className="lg:w-2/12 xxl-w-1/6 md:flex-shrink-0">
           <div className="navbar-start lg:hidden">
             <div className="dropdown">
@@ -210,187 +399,4 @@ const DashBoard = () => {
         <div className="md:flex-1 overflow-x-auto px-5">
           <Outlet></Outlet>
         </div>
-      </div>
-    </>
-  );
-};
-
-export default DashBoard;
-
-// {isLoading ? (
-//   <UniLoader />
-// ) : (
-//   <div className="md:flex">
-//     <div className="lg:w-2/12 xxl-w-1/6 md:flex-shrink-0">
-//       <div className="navbar-start lg:hidden">
-//         <div className="dropdown">
-//           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               className="h-5 w-5"
-//               fill="none"
-//               viewBox="0 0 24 24"
-//               stroke="red"
-//             >
-//               <path
-//                 strokeLinecap="round"
-//                 strokeLinejoin="round"
-//                 strokeWidth="2"
-//                 d="M4 6h16M4 12h8m-8 6h16"
-//               />
-//             </svg>
-//           </label>
-//           <ul
-//             tabIndex={0}
-//             className=" menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-blue-600 rounded-box  w-52 "
-//           >
-//             {role === "candidate" ? (
-//               <>
-//                 <li>
-//                   <h2 className="text-xl xl:text-2xl text-red-800 font-bold bg-white my-2">
-//                     Candidate DashBoard
-//                   </h2>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/profile">Profile</NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/resume">Resume</NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/appliedJobs">
-//                     Applied Jobs
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/bookmarks">Bookmarks</NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/analytics">Analytics</NavLink>
-//                 </li>
-//               </>
-//             ) : (
-//               <>
-//                 <li>
-//                   <h2 className="text-2xl text-blue-800 font-bold bg-white my-2">
-//                     Company DashBoard
-//                   </h2>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/companyProfile">
-//                     Profile
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/postJob">Post Job</NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/postedJobs">Posted Job</NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/companyAnalytics">
-//                     Analytics
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink to="/dashboard/employees">Employees</NavLink>
-//                 </li>
-//               </>
-//             )}
-//             <div className="divider"></div>
-
-//             <li>
-//               <NavLink to="/">Home</NavLink>
-//             </li>
-//             <li>
-//               <NavLink to="/availableCamp">Available Camp</NavLink>
-//             </li>
-//             <li>
-//               <button
-//                 onClick={handleLogout}
-//                 className="flex items-center btn btn-sm"
-//               >
-//                 <BiLogOut className="text-2xl" />
-//                 <span>Sign Out</span>
-//               </button>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//       <div className="lg:flex hidden  min-h-screen bg-gray-200 pt-12 lg:sticky  lg:top-0 lg:inset-x-0 lg:z-20">
-//         <ul className="menu text-xl">
-//           {role === "candidate" ? (
-//             <>
-//               <li>
-//                 <h2 className="text-2xl text-blue-800 font-bold bg-white my-2">
-//                   Candidate DashBoard
-//                 </h2>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/candidateProfile">
-//                   Profile
-//                 </NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/resume">Resume</NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/appliedJobs">
-//                   Applied Jobs
-//                 </NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/bookmarks">Bookmarks</NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/candidateAnalytics">
-//                   Analytics
-//                 </NavLink>
-//               </li>
-//             </>
-//           ) : (
-//             <>
-//               <li>
-//                 <h2 className="text-2xl text-red-800 font-bold bg-white my-2">
-//                   Company DashBoard
-//                 </h2>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/companyProfile">Profile</NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/postJob">Post Job</NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/postedJobs">Posted Job</NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/companyAnalytics">
-//                   Analytics
-//                 </NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to="/dashboard/employees">Employees</NavLink>
-//               </li>
-//             </>
-//           )}
-
-//           <div className="divider"></div>
-
-//           <li>
-//             <NavLink to="/">Home</NavLink>
-//           </li>
-//           <li>
-//             <button onClick={handleLogout} className="flex items-center">
-//               <BiLogOut className="text-2xl" />
-//               <span>Sign Out</span>
-//             </button>
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//     <div className="md:flex-1 overflow-x-auto px-5">
-//       <Outlet></Outlet>
-//     </div>
-//   </div>
-// )}
+      </div> */}
