c3baa349925e2ff944956aad60e775b06545e89d
nasimrifat101 <nasimrifat101@gmail.com>
2024-02-06 13:39:38 +0600

    [Feature/Modify] Company can now see their posted jobs

--- src/Company/PostedJobs.tsx
+++ src/Company/PostedJobs.tsx
@@ -3,28 +3,43 @@ import PostedJobsCard from "./PostedJobsCard";
 import Job from "../Pages/Home/PopularJobs/Job";
 import useAuth from "../hooks/useAuth";
 import useAxiosPublic from "../hooks/useAxiosPublic";
+import CandidateNav from "../Candidate/CommonNavbar/CandidateNav";
+import { useEffect, useState } from "react";
 
 const PostedJobs = () => {
   const axiosPublic = useAxiosPublic();
   const { user } = useAuth();
   console.log(user?.email);
-  const { data: popularJobs = [], refetch } = useQuery({
-    queryKey: ["popularJobs"],
-    queryFn: async () => {
-      const res = await axiosPublic.get(`/all-job-posts`);
-      return res.data;
-    },
-  });
-  console.log(popularJobs);
-  const companyPostedJobs = popularJobs?.filter(
-    (job: { companyEmail: string }) => job.companyEmail === user?.email
-  );
+  const [companyPostedJobs, setCompanyPostedJobs] = useState<any | null>(null);
+
+  useEffect(() => {
+    if (user?.email) {
+      axiosPublic
+        .get(`/get-company-posted-jobs/${user?.email}`)
+        .then((res) => {
+          setCompanyPostedJobs(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const length = companyPostedJobs?.length
 
   return (
-    <div className="flex flex-col  items-center">
-      {companyPostedJobs.map((job: Job) => (
-        <PostedJobsCard key={job._id} job={job} refetch={refetch} />
-      ))}
+    <div>
+      <CandidateNav
+        text="Your Posted Jobs"
+        btn={length}
+        btn2=""
+        handleClick={() => {}}
+        handleClick2={() => {}}
+      />
+      <div className="grid grid-cols-4 gap-6">
+        {companyPostedJobs?.map((job: Job) => (
+          <PostedJobsCard key={job._id} job={job} />
+        ))}
+      </div>
     </div>
   );
 };
