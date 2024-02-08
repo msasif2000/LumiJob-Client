f1c4747752b224cb8b881f66ad7c4babf6c9acd1
Rakibul Islam <mdrakib.5627@gmail.com>
2024-02-08 10:07:47 +0600

    option subscription page responsive

--- src/Pages/Subscription/OptionSubscribe.tsx
+++ src/Pages/Subscription/OptionSubscribe.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const OptionSubscribe: React.FC = () => {
   return (
-    <div className="bg-gradient-to-r from-[#fdd5d5] via-[#f0f0f0] to-[#cfffcd] min-h-screen">
+    <div className="bg-gradient-to-r from-[#f0f0f0] via-[#fdf5f5] to-[#fffbfb] min-h-screen">
       <div className="max-w-screen-2xl mx-auto">
         <div className="hero min-h-screen">
           <div className="hero-content text-center">
@@ -11,10 +11,10 @@ const OptionSubscribe: React.FC = () => {
               <h1 className="text-3xl font-bold">
                 What kind of packages you want to see?
               </h1>
-              <div className="flex justify-center space-x-5">
+              <div className="md:flex justify-center space-x-5">
 
                 <Link to={"/subscriptionsUiCompany"}>
-                  <div className="card card-compact w-96 bg-base-100 hover:shadow-xl duration-500 ease-linear cursor-pointer">
+                  <div className="card card-compact w-full md:w-96 bg-base-100 hover:shadow-xl duration-500 ease-linear cursor-pointer">
                     <figure>
                       <img
                         src="https://i.postimg.cc/PJnXCF5v/1904-i402-011-Web-development-isometric-concept-infographics.jpg"
@@ -29,7 +29,7 @@ const OptionSubscribe: React.FC = () => {
                 </Link>
 
                 <Link to={"/subscriptionsUiCandidate"}>
-                  <div className="card card-compact w-96 bg-base-100 hover:shadow-xl duration-500 ease-linear cursor-pointer">
+                  <div className="card card-compact md:w-96 bg-base-100 hover:shadow-xl duration-500 ease-linear cursor-pointer">
                     <figure>
                       <img
                         src="https://i.postimg.cc/LXm0Ct5g/18771.jpg"
