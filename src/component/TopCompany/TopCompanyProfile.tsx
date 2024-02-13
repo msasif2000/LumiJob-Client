6f4c4acf23a35ab0639b17cda3615576888c6008
Foysal79 <onthor312001@gmail.com>
2024-02-13 16:07:15 +0600

    top recomunded company details text dainamicly set

--- src/component/TopCompany/TopCompanyProfile.tsx
+++ src/component/TopCompany/TopCompanyProfile.tsx
@@ -25,7 +25,8 @@ const TopCompanyProfile = () => {
             {/* company details pages  */}
             <div>
             <div className=" pt-10 pb-16  px-7 bg-gradient-to-r from-[#228bedde] to-[#6565C7]">
-            <p className=" pb-8 lg:text-4xl font-bold text-center text-white ">Company Profile {`>`} {CompanyProfile?.name} </p>
+            <p className=" pb-8 lg:text-4xl font-bold text-center text-white ">Company Profile {`>`} 
+            {CompanyProfile?.name} </p>
           {/* company information */}
            <div className="flex gap-10 lg:px-16 lg:pt-10">
            {/* company title img adreess ect */}
@@ -64,10 +65,10 @@ const TopCompanyProfile = () => {
 
 
             {/* company details */}
-            <div className="flex-1 bg-[#D3D3D3]">
-                <p className="text-sm md:text-base font-semibold uppercase text-black mb-2">Details</p>
-                <p className="text-black text-sm md:text-base ">
-                Established on March 21, 2006, GreenGrowth Ventures is a leading force in the social media industry, headquartered in San Francisco, CA, USA. With a focus on leveraging technology for positive societal impact, our company is registered under Company No. 876. Specializing in fostering digital connectivity and community empowerment, we offer innovative solutions to meet evolving needs. Reach out to us at +97343356789 or info@greengrowthventures.com for inquiries or collaborations. Visit our website https://twitter.com/ to discover our diverse range of initiatives aimed at driving meaningful change in the digital landscape. Join us in shaping a brighter future through the power of social media.
+            <div className="flex-1 bg-[#ffffff] pt-16 px-10 rounded-2xl">
+                <p className="text-sm md:text-base font-semibold  text-black mb-5 uppercase">Information</p>
+                <p className=" text-sm md:text-base text-justify text-[#808285] ">
+                Established on {CompanyProfile?.founding}, {CompanyProfile?.name} is a leading force in the {CompanyProfile?.industry} industry, headquartered in {CompanyProfile?.city} - {CompanyProfile?.postal}, {CompanyProfile?.country}. With a focus on leveraging technology for positive societal impact, our company is registered under Company No. {CompanyProfile?.registration}. Specializing in fostering digital connectivity and community empowerment, we offer innovative solutions to meet evolving needs. Reach out to us at {CompanyProfile?.phone} or {CompanyProfile?.email} for inquiries or collaborations. Visit our website {CompanyProfile?.website} to discover our diverse range of initiatives aimed at driving meaningful change in the digital landscape. Join us in shaping a brighter future through the power of {CompanyProfile?.industry}.
                 </p>
                 
             </div>
