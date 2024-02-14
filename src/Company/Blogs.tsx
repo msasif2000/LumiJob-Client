74cc6e9a904d887d57d9ad1b5ec079e87ae903ad
Asif <msasifiiuc@gmail.com>
2024-02-14 21:50:45 +0600

    blog card component created, API created

--- src/Company/Blogs.tsx
+++ src/Company/Blogs.tsx
@@ -1,10 +1,48 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import CandidateNav from "../Candidate/CommonNavbar/CandidateNav";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
+import useAxiosPublic from "../hooks/useAxiosPublic";
+import BlogCard from "./BlogCard";
 
 const Blogs = () => {
+    const navigate = useNavigate();
+    const [companyPostedBlogs, setCompanyPostedBlogs] = useState();
+    const {user} = useAuth();
+    const axiosPublic = useAxiosPublic();
+
+    useEffect(() => {
+        if(user?.email) {
+            axiosPublic.get(`get-posted-blogs/${user.email}`)
+            .then(res => {
+                setCompanyPostedBlogs(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }, [user]);
+
+    const handleBlogPosts = () =>{
+        navigate('/dashboard/post-a-blog')
+      }
     return (
         <div>
-            <Link to="/dashboard/post-a-blog"><button>Post Blog</button></Link>
-        </div>
+      <CandidateNav
+        text="Your Posted Jobs"
+        btn="Post Jobs"
+        btn2={length}
+        handleClick={() => {handleBlogPosts()}}
+        handleClick2={() => {}}
+      />
+      <div className="grid grid-cols-4 gap-6">
+        {companyPostedBlogs?.map((job: Job) => (
+          <BlogCard key={job._id} job={job} handleDelete={handleDelete} />
+        ))}
+      </div>
+      <ToastContainer/>
+    </div>
     );
 };
 
