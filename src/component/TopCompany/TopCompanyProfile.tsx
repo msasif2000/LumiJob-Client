5f9708718654c24e7d1043d6d3e2d3cc3fea6421
Foysal79 <onthor312001@gmail.com>
2024-02-15 08:57:59 +0600

    top recommunded company page tab session font edite

--- src/component/TopCompany/TopCompanyProfile.tsx
+++ src/component/TopCompany/TopCompanyProfile.tsx
@@ -3,6 +3,7 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { useEffect, useState } from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
+
 const TopCompanyProfile = () => {
   const { id } = useParams<{ id: string }>();
   const axiosPublic = useAxiosPublic();
@@ -20,16 +21,15 @@ const TopCompanyProfile = () => {
 
   return (
     <div className="">
-      
       <div>
         {/* company details pages  */}
-        <div className=" pt-10 pb-16  px-7 bg-[#4869DD]">
+        <div className=" pt-5 pb-5  px-7 bg-[#4360ca]">
           <p className=" pb-8 lg:text-4xl font-bold text-center text-white  ">
             Company Profile {`> `}
             {CompanyProfile?.name}{" "}
           </p>
           {/* company information */}
-          <div className="flex gap-10 lg:px-16 lg:pt-10">
+          <div className="flex gap-10 lg:px-16 lg:pt-2">
             {/* company title img adreess ect */}
             <div className="flex-1">
               <div className="flex gap-4 md:gap-10 mb-10">
@@ -106,109 +106,83 @@ const TopCompanyProfile = () => {
           </div>
         </div>
 
-
-        <div>
-        <Tabs>
-    <TabList>
-      <Tab>Mario</Tab>
-      <Tab disabled>Luigi</Tab>
-      <Tab>Peach</Tab>
-      <Tab>Yoshi</Tab>
-      <Tab>Toad</Tab>
-    </TabList>
-
-    <TabPanel>
-      <p>
-        <b>Mario</b> (<i>Japanese: マリオ Hepburn: Mario, [ma.ɾʲi.o]</i>) (<i>English:
-        /ˈmɑːrioʊ/; Italian: [ˈmaːrjo]</i>) is a fictional character in the Mario video
-        game franchise, owned by Nintendo and created by Japanese video game designer
-        Shigeru Miyamoto. Serving as the company's mascot and the eponymous protagonist
-        of the series, Mario has appeared in over 200 video games since his creation.
-        Depicted as a short, pudgy, Italian plumber who resides in the Mushroom
-        Kingdom, his adventures generally center upon rescuing Princess Peach from the
-        Koopa villain Bowser. His younger brother and sidekick is Luigi.
-      </p>
-      <p>
-        Source:{' '}
-        <a href="https://en.wikipedia.org/wiki/Mario" target="_blank">
-          Wikipedia
-        </a>
-      </p>
-    </TabPanel>
-    <TabPanel>
-      <p>
-        <b>Luigi</b> (<i>Japanese: ルイージ Hepburn: Ruīji, [ɾɯ.iː.dʑi̥]</i>) (<i>English: /luˈiːdʒi/;
-        Italian: [luˈiːdʒi]</i>) is a fictional character featured in video games and related media
-        released by Nintendo. Created by prominent game designer Shigeru Miyamoto, Luigi is portrayed
-        as the slightly younger but taller fraternal twin brother of Nintendo's mascot Mario, and
-        appears in many games throughout the Mario franchise, often as a sidekick to his brother.
-      </p>
-      <p>
-        Source:{' '}
-        <a href="https://en.wikipedia.org/wiki/Luigi" target="_blank">
-          Wikipedia
-        </a>
-      </p>
-    </TabPanel>
-    <TabPanel>
-      <p>
-        <b>Princess Peach</b> (<i>Japanese: ピーチ姫 Hepburn: Pīchi-hime, [piː.tɕi̥ çi̥.me]</i>)
-        is a character in Nintendo's Mario franchise. Originally created by Shigeru Miyamoto,
-        Peach is the princess of the fictional Mushroom Kingdom, which is constantly under
-        attack by Bowser. She often plays the damsel in distress role within the series and
-        is the lead female. She is often portrayed as Mario's love interest and has appeared
-        in Super Princess Peach, where she is the main playable character.
-      </p>
-      <p>
-        Source:{' '}
-        <a href="https://en.wikipedia.org/wiki/Princess_Peach" target="_blank">
-          Wikipedia
-        </a>
-      </p>
-    </TabPanel>
-    <TabPanel>
-      <p>
-        <b>Yoshi</b> (<i>ヨッシー Yosshī, [joɕ.ɕiː]</i>) (<i>English: /ˈjoʊʃi/ or /ˈjɒʃi/</i>), once
-        romanized as Yossy, is a fictional anthropomorphic dinosaur who appears in
-        video games published by Nintendo. Yoshi debuted in Super Mario World (1990) on the
-        Super Nintendo Entertainment System as Mario and Luigi's sidekick. Yoshi later starred
-        in platform and puzzle games, including Super Mario World 2: Yoshi's Island, Yoshi's Story
-        and Yoshi's Woolly World. Yoshi also appears in many of the Mario spin-off games, including
-        Mario Party and Mario Kart, various Mario sports games, and Nintendo's crossover fighting
-        game series Super Smash Bros. Yoshi belongs to the species of the same name, which is
-        characterized by their variety of colors.
-      </p>
-      <p>
-        Source:{' '}
-        <a href="https://en.wikipedia.org/wiki/Yoshi" target="_blank">
-          Wikipedia
-        </a>
-      </p>
-    </TabPanel>
-    <TabPanel>
-      <p>
-        <b>Toad</b> (<i>Japanese: キノピオ Hepburn: Kinopio</i>) is a fictional character who primarily
-        appears in Nintendo's Mario franchise. Created by Japanese video game designer Shigeru Miyamoto,
-        he is portrayed as a citizen of the Mushroom Kingdom and is one of Princess Peach's most loyal
-        attendants; constantly working on her behalf. He is usually seen as a non-player character (NPC)
-        who provides assistance to Mario and his friends in most games, but there are times when Toad(s)
-        takes center stage and appears as a protagonist, as seen in Super Mario Bros. 2, Wario's Woods,
-        Super Mario 3D World, and Captain Toad: Treasure Tracker.
-      </p>
-      <p>
-        Source:{' '}
-        <a href="https://en.wikipedia.org/wiki/Toad_(Nintendo)" target="_blank">
-          Wikipedia
-        </a>
-      </p>
-    </TabPanel>
-  </Tabs>
-             
-        </div>
+        
+        
       </div>
+      {/* tab sessions */}
+      <div className="w-10/12 mx-auto" >
+          <Tabs>
+            <TabList className={`text-2xl font-bold text-center`}>
+              <Tab >Posted Jobs</Tab>
+              <Tab>Blog</Tab>
+              <Tab>Seminar</Tab>
+            </TabList>
+
+            <TabPanel>
+              <p>
+                <b>Mario</b> (<i>Japanese: マリオ Hepburn: Mario, [ma.ɾʲi.o]</i>
+                ) (<i>English: /ˈmɑːrioʊ/; Italian: [ˈmaːrjo]</i>) is a
+                fictional character in the Mario video game franchise, owned by
+                Nintendo and created by Japanese video game designer Shigeru
+                Miyamoto. Serving as the company's mascot and the eponymous
+                protagonist of the series, Mario has appeared in over 200 video
+                games since his creation. Depicted as a short, pudgy, Italian
+                plumber who resides in the Mushroom Kingdom, his adventures
+                generally center upon rescuing Princess Peach from the Koopa
+                villain Bowser. His younger brother and sidekick is Luigi.
+              </p>
+              <p>
+                Source:{" "}
+                <a href="https://en.wikipedia.org/wiki/Mario" target="_blank">
+                  Wikipedia
+                </a>
+              </p>
+            </TabPanel>
+            <TabPanel>
+              <p>
+                <b>Luigi</b> (
+                <i>Japanese: ルイージ Hepburn: Ruīji, [ɾɯ.iː.dʑi̥]</i>) (
+                <i>English: /luˈiːdʒi/; Italian: [luˈiːdʒi]</i>) is a fictional
+                character featured in video games and related media released by
+                Nintendo. Created by prominent game designer Shigeru Miyamoto,
+                Luigi is portrayed as the slightly younger but taller fraternal
+                twin brother of Nintendo's mascot Mario, and appears in many
+                games throughout the Mario franchise, often as a sidekick to his
+                brother.
+              </p>
+              <p>
+                Source:{" "}
+                <a href="https://en.wikipedia.org/wiki/Luigi" target="_blank">
+                  Wikipedia
+                </a>
+              </p>
+            </TabPanel>
+            <TabPanel>
+              <p>
+                <b>Princess Peach</b> (
+                <i>Japanese: ピーチ姫 Hepburn: Pīchi-hime, [piː.tɕi̥ çi̥.me]</i>)
+                is a character in Nintendo's Mario franchise. Originally created
+                by Shigeru Miyamoto, Peach is the princess of the fictional
+                Mushroom Kingdom, which is constantly under attack by Bowser.
+                She often plays the damsel in distress role within the series
+                and is the lead female. She is often portrayed as Mario's love
+                interest and has appeared in Super Princess Peach, where she is
+                the main playable character.
+              </p>
+              <p>
+                Source:{" "}
+                <a
+                  href="https://en.wikipedia.org/wiki/Princess_Peach"
+                  target="_blank"
+                >
+                  Wikipedia
+                </a>
+              </p>
+            </TabPanel>
+          </Tabs>
+        </div>
 
       {/* company posted jobs */}
-      
     </div>
   );
 };
