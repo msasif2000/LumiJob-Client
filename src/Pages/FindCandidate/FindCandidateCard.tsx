7b32baa82a961d31b18a1f7416e16e69438d8072
Rakibul Islam <mdrakib.5627@gmail.com>
2024-02-15 10:49:22 +0600

    layout responsive

--- src/Pages/FindCandidate/FindCandidateCard.tsx
+++ src/Pages/FindCandidate/FindCandidateCard.tsx
@@ -9,19 +9,19 @@ interface CandidateCardsProps {
 
 const FindCandidateCard: React.FC<CandidateCardsProps> = ({ candidate }) => {
 
-    const { _id, photo, name, position, city , country } = candidate;
+    const { _id, photo, name, position, city, country } = candidate;
 
     return (
         <div className="">
             <div>
                 <div className="group grid grid-cols-12 overflow-hidden rounded-lg border hover:border-accent py-2 text-gray-700 bg-white ">
-                    <div className="order-2 col-span-2 -ml-14 text-left text-gray-600 hover:text-gray-700 sm:-order-1 sm:ml-4">
-                        <div className="group relative h-16 w-16 overflow-hidden rounded-lg">
+                    <div className=" col-span-3 md:col-span-2 text-left text-gray-600 hover:text-gray-700  sm:ml-4">
+                        <div className="group relative rounded-full h-12 w-12 md:h-16 md:w-16 overflow-hidden">
                             <img src={photo} alt="logo" className="w-full  text-gray-700" />
                         </div>
                     </div>
                     <div className="col-span-7 flex flex-col pr-8 text-left sm:pl-4">
-                        
+
                         <div className="flex justify-between">
                             <h3 className="mb-1 overflow-hidden pr-7 text-xl font-semibold sm:text-xl">
                                 {name}
@@ -29,7 +29,7 @@ const FindCandidateCard: React.FC<CandidateCardsProps> = ({ candidate }) => {
                         </div>
                         <p className="text-sm opacity-90">{position}</p>
                         <h3 className="text-xs text-gray-600 text-opacity-90 mb-1">
-                        {city}, {country}</h3>
+                            {city}, {country}</h3>
 
                         {/* <div className="flex items-center justify-center mt-4 gap-2 md:hidden">
                             <BookmarkButton job={job} />
@@ -40,9 +40,9 @@ const FindCandidateCard: React.FC<CandidateCardsProps> = ({ candidate }) => {
                             </Link>
                         </div> */}
                     </div>
-                    <div>
-                    <Link className=" col-span-3" 
-                        to={`/candidate-detailsProfile/${_id}`}>
+                    <div className=" col-span-2 md:col-span-3">
+                        <Link className=" "
+                            to={`/candidate-detailsProfile/${_id}`}>
                             <button className="mt-4 py-1 px-8 border border-gray-300 hover:bg-accent hover:text-white font-semibold text-base rounded-lg">
                                 Profile
                             </button>
