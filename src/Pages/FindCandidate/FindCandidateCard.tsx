4cfc043bed2c770f375d02a17e8b31f59b116c9f
Rakibul Islam <mdrakib.5627@gmail.com>
2024-02-15 11:08:19 +0600

    button responsive

--- src/Pages/FindCandidate/FindCandidateCard.tsx
+++ src/Pages/FindCandidate/FindCandidateCard.tsx
@@ -1,6 +1,7 @@
 
 import { Link } from "react-router-dom";
 import Candidate from "./CandidateType";
+import { FaPlusSquare } from "react-icons/fa";
 
 interface CandidateCardsProps {
     candidate: Candidate;
@@ -16,11 +17,11 @@ const FindCandidateCard: React.FC<CandidateCardsProps> = ({ candidate }) => {
             <div>
                 <div className="group grid grid-cols-12 overflow-hidden rounded-lg border hover:border-accent py-2 text-gray-700 bg-white ">
                     <div className=" col-span-3 md:col-span-2 text-left text-gray-600 hover:text-gray-700  sm:ml-4">
-                        <div className="group relative rounded-full h-12 w-12 md:h-16 md:w-16 overflow-hidden">
+                        <div className="group relative rounded-lg h-12 w-12 md:h-16 md:w-16 overflow-hidden">
                             <img src={photo} alt="logo" className="w-full  text-gray-700" />
                         </div>
                     </div>
-                    <div className="col-span-7 flex flex-col pr-8 text-left sm:pl-4">
+                    <div className="col-span-8 md:col-span-7 flex flex-col pr-8 text-left sm:pl-4">
 
                         <div className="flex justify-between">
                             <h3 className="mb-1 overflow-hidden pr-7 text-xl font-semibold sm:text-xl">
@@ -40,12 +41,14 @@ const FindCandidateCard: React.FC<CandidateCardsProps> = ({ candidate }) => {
                             </Link>
                         </div> */}
                     </div>
-                    <div className=" col-span-2 md:col-span-3">
+                    <div className=" col-span-1 md:col-span-3">
                         <Link className=" "
                             to={`/candidate-detailsProfile/${_id}`}>
-                            <button className="mt-4 py-1 px-8 border border-gray-300 hover:bg-accent hover:text-white font-semibold text-base rounded-lg">
+                            <button className="hidden md:block mt-4 py-1 px-8 border border-gray-300 hover:bg-accent hover:text-white font-semibold text-base rounded-lg">
                                 Profile
                             </button>
+                            <button><p className=" mt-4 md:hidden"> <FaPlusSquare /></p></button>
+                           
                         </Link>
                     </div>
                 </div>
