64f7c83d8000c0bfa97831d023dbdad2be78d6b9
Foysal79 <onthor312001@gmail.com>
2024-02-20 00:57:23 +0600

    jumijobs home pasge implimentions trusted by jobs session add strucure

---
+++ src/Pages/Home/TrustedByCompany/TrustedByCompany.tsx
@@ -0,0 +1,11 @@
+
+
+const TrustedByCompany = () => {
+    return (
+        <div className="w-full h-36 bg-[#ffe4e4]" >
+            
+        </div>
+    );
+};
+
+export default TrustedByCompany;
