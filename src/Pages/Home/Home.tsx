093c0eb42b0b8018b1e1893f83eff6d830427914
nasimrifat101 <nasimrifat101@gmail.com>
2024-02-19 14:30:58 +0600

    [dnd fixed] api endspoint created

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,14 +3,11 @@ import Banner from "../../component/Banner/Banner";
 import PopularJobs from "./PopularJobs/PopularJobs";
 import HowItWorks from "./HowItWorks/HowItWorks";
 
-// import BannerRe from "../../component/Banner/BannerRe";
-
 import TopCompany from "../../component/TopCompany/TopCompany";
 
 const Home = () => {
   return (
     <div>
-      {/* <BannerRe /> */}
       <Banner />
       <HowItWorks />
       {/* <Step></Step> */}
