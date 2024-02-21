49594eb4a842c7060894c82505f587375466e860
Asif <msasifiiuc@gmail.com>
2024-02-21 17:16:14 +0600

    photo and name  added to dashboard

--- src/Layout/DashBoard.tsx
+++ src/Layout/DashBoard.tsx
@@ -1,3 +1,4 @@
+import { RxAvatar } from "react-icons/rx";
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import { BiLogOut } from "react-icons/bi";
@@ -11,8 +12,10 @@ const DashBoard = () => {
   const [isLoading, setLoading] = useState(true);
   const axiosPublic = useAxiosPublic();
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [userData, setUserData] = useState({} as any);
   const [role, setRole] = useState("");
 
+  console.log(user);
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 1024 && isNavOpen) {
@@ -33,6 +36,7 @@ const DashBoard = () => {
         .then((res) => {
           // console.log(res.data.role);
           setRole(res.data.role);
+          setUserData(res.data);
           setLoading(false);
         })
         .catch((error) => {
@@ -175,6 +179,20 @@ const DashBoard = () => {
                   </h3>
                 </Link>
                 <div className="divider mt-[0.7rem]"></div>
+                <div className="flex items-center m-4 gap-2">
+                  <div>
+                    {
+                      userData?.photo ?
+                        <img src={userData.photo} alt="" className="rounded-full h-10 w-10"/>
+                        :
+                        <RxAvatar className="text-2xl" />
+                    }
+                  </div>
+                  <div>
+                    <h6 className="text-center capitalize text-accent">{userData?.name || "Your name"}</h6>
+                  </div>
+                </div>
+
                 <ul className="menu text-xl">{Links}</ul>
               </div>
               <div>
