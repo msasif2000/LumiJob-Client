d6b3271a14e9d8409bd51fa0a3bda8d61bef4573
Rakibul Islam <mdrakib.5627@gmail.com>
2024-02-22 20:00:59 +0600

    pagination responsive on findjob with logic change

--- src/component/Shared/BookmarkButton.tsx
+++ src/component/Shared/BookmarkButton.tsx
@@ -122,7 +122,7 @@ const BookmarkButton: React.FC<BookmarkButtonProps> = ({ job }) => {
     <button
       onClick={handleAddToBookmark}
       title={isBookmarked ? "Remove from bookmarks" : "Add to bookmarks"}
-      className={` border p-2 border-black rounded-lg hover:text-accent hover:border-accent ${isBookmarked ? "bg-blue-400" : ""}`}
+      className={` border p-2 border-black rounded-lg hover:text-accent hover:border-accent ${isBookmarked ? "bg-blue-300" : ""}`}
     >
       <MdBookmarkAdd />
     </button>
