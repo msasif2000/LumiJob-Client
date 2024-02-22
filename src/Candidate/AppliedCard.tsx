61c1df09ffcbd0f97172d5add2ee0385b84eae71
nasimrifat101 <nasimrifat101@gmail.com>
2024-02-22 23:50:33 +0600

    [Feature] User can now remove applied jobs

--- src/Candidate/AppliedCard.tsx
+++ src/Candidate/AppliedCard.tsx
@@ -134,7 +134,7 @@ const AppliedCard: React.FC<prop> = ({ job, handleDelete }) => {
       {showConfirmation && (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-md shadow-2xl w-60">
           <p className="text-md font-semibold">
-            Are you sure you want to cancel this application?
+            Are you sure you want to cancel this job application?
           </p>
           <div className="flex justify-center mt-4">
             <button
