db578c9aeb29afb7e5951c0ffcc94adef3de9ba0
Foysal79 <onthor312001@gmail.com>
2024-02-23 20:36:15 +0600

    company feedback showing candited applied jobs

--- src/Candidate/AppliedCard.tsx
+++ src/Candidate/AppliedCard.tsx
@@ -135,9 +135,19 @@ const AppliedCard: React.FC<prop> = ({ job, handleDelete }) => {
         </div>
       )}
       {cancel && (
-        <div className="absolute top-10 -right-8">
-          <TbMessage className="text-green-500 cursor-pointer transition duration-300 ease-in-out transform hover:scale-150"
+        <div className="absolute top-10 lg:-right-8 xl:right-1">
+          <TbMessage onClick={()=>document.getElementById('my_modal_3').showModal()} className="text-green-500 cursor-pointer transition duration-300 ease-in-out transform hover:scale-150"
             />
+            <dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <h3 className="font-bold text-lg">Hello!</h3>
+    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+  </div>
+</dialog>
           
         </div>
       )}
