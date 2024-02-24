f012a761423ecc4bc5b573b6d651abf6375835df
nasimrifat101 <nasimrifat101@gmail.com>
2024-02-24 17:43:18 +0600

    [Bug fix] Fixed bug and codebase is now clean

--- src/Company/CompanyAnalytics.tsx
+++ src/Company/CompanyAnalytics.tsx
@@ -125,10 +125,12 @@ const CompanyAnalytics: FunctionComponent = () => {
                                 fill="#8884d8"
                                 dataKey="value"
                             >
-                                {pieChartData.map((entry, index) => (
+                                {pieChartData.map((entry: any, index: number) => (
                                     <Cell
                                         key={`cell-${index}`}
                                         fill={colors[index % colors.length]} // Assign predefined colors
+                                        {...entry}
+                                       
                                     />
                                 ))}
                             </Pie>
