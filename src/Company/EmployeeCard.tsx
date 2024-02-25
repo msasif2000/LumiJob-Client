eed206d1358d8ede39536b8ee075e26de42e8bd1
Sejan~ <md4sejan@gmail.com>
2024-02-26 01:38:20 +0600

    fetch selectedApplicants data, show data dynamically on EmployeeCard and type checked using typescript

--- src/Company/EmployeeCard.tsx
+++ src/Company/EmployeeCard.tsx
@@ -1,27 +1,41 @@
 import { MdArrowOutward } from "react-icons/md";
+import Employee from "./Employee";
+import { Link } from "react-router-dom";
 
-const EmployeeCard = () => {
+interface employeeProps {
+  employee: Employee;
+}
+
+const EmployeeCard: React.FC<employeeProps> = ({ employee }) => {
+  const { name, position, profile, _id } = employee;
   return (
     <div className="w-full  bg-white border border-gray-200 rounded-lg ">
       <div className="flex justify-end px-4 pt-4">
-        <button className="py-2 px-3 ms-2 text-xs font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
-          <p className="flex items-center justify-center gap-2 ">
-            Profile
-            <MdArrowOutward />
-          </p>
-        </button>
+        <Link to={`/candidate-detailsProfile/${_id}`}>
+          <button className="py-2 px-3 ms-2 text-xs font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+            <p className="flex items-center justify-center gap-2 ">
+              Profile
+              <MdArrowOutward />
+            </p>
+          </button>
+        </Link>
       </div>
       <div className="flex flex-col items-center pb-10">
         <figure>
           <img
             alt="Bonnie image"
             className="w-24 h-24 mb-3 rounded-full object-cover shadow-lg"
-            src="https://i.pinimg.com/564x/97/bb/06/97bb067e30ff6b89f4fbb7b9141025ca.jpg"
+            src={
+              profile
+                ? profile
+                : "https://i.pinimg.com/564x/97/bb/06/97bb067e30ff6b89f4fbb7b9141025ca.jpg"
+            }
           />
         </figure>
-        <h5 className="mb-1 text-xl font-medium text-gray-900">Bonnie Green</h5>
-        <span className="text-sm text-gray-500">Visual Designer</span>
+        <h5 className="mb-1 text-xl font-medium text-gray-900">{name}</h5>
+        <span className="text-sm text-gray-500">{position}</span>
       </div>
+      <div></div>
     </div>
   );
 };
--- src/Company/Employees.tsx
+++ src/Company/Employees.tsx
@@ -1,22 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 import EmployeeCard from "./EmployeeCard";
+import Employee from "./Employee";
+import useAuth from "../hooks/useAuth";
 
 const Employees = () => {
   const axiosPublic = useAxiosPublic();
+  const { user } = useAuth();
 
   const { data: employees = [] } = useQuery({
     queryKey: ["employees"],
     queryFn: async () => {
-      const res = await axiosPublic.get(`/selectedApplicants`);
+      const res = await axiosPublic.get(
+        `/selectedApplicants?companiEmail=${user.email}`
+      );
       return res.data;
     },
   });
 
   return (
     <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 py-6">
-      {employees?.map((employee: any) => (
-        <EmployeeCard key={employee._id} />
+      {employees?.map((employee: Employee) => (
+        <EmployeeCard key={employee._id} employee={employee} />
       ))}
     </div>
   );
