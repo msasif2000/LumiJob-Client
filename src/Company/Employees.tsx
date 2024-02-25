0a0e457e84b3203fffb3670f6ce7485b3cc15410
Sejan~ <md4sejan@gmail.com>
2024-02-25 16:47:05 +0600

    fetch selected applicants data and show employee card

--- src/Company/Employees.tsx
+++ src/Company/Employees.tsx
@@ -1,14 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 import EmployeeCard from "./EmployeeCard";
 
 const Employees = () => {
+  const axiosPublic = useAxiosPublic();
+
+  const { data: employees = [] } = useQuery({
+    queryKey: ["employees"],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/selectedApplicants`);
+      return res.data;
+    },
+  });
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 py-6">
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
-      <EmployeeCard />
+    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 py-6">
+      {employees?.map((employee: any) => (
+        <EmployeeCard key={employee._id} />
+      ))}
     </div>
   );
 };
