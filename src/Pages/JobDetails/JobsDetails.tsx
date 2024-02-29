93c72089d0cedb7bc860033f71ab79df65dc1815
nasimrifat101 <nasimrifat101@gmail.com>
2024-02-29 17:38:58 +0600

    [Applied cards changed injob detailspage]

--- src/Pages/JobDetails/JobsDetails.tsx
+++ src/Pages/JobDetails/JobsDetails.tsx
@@ -621,7 +621,7 @@ const JobsDetails: React.FC = () => {
                   <div className="sm:flex sm:items-start">
                     <div
                       {...getRootProps()}
-                      className="dropzone bg-violet-100 hover:bg-violet-300 duration-1000 p-10 w-full rounded-2xl cursor-pointer"
+                      className="dropzone bg-blue-100 hover:bg-blue-300 duration-1000 p-10 w-full rounded-2xl cursor-pointer"
                     >
                       <input {...getInputProps()} />
 
