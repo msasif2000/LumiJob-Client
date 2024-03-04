25770144b30ad09c3f99dcd2d24d117087ea8236
Rakibul Islam <mdrakib.5627@gmail.com>
2024-03-04 22:15:21 +0600

    text responsive

--- src/component/Footer/FooterPages/TermsConditions.tsx
+++ src/component/Footer/FooterPages/TermsConditions.tsx
@@ -14,11 +14,11 @@ const TermsConditions: React.FC = () => {
                 <p className="text-xs md:text-lg lg:text-xl mb-4">
                     The following terminology applies to these Terms and Conditions, Privacy Statement, and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance, and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of the provision of the Company's stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization, and/or he/she or they, are taken as interchangeable and therefore as referring to same.
                 </p>
-                <h2 className="text-2xl font-semibold mb-4">License</h2>
+                <h2 className="text:xs md:text-lg lg:text-2xl font-semibold mb-4">License</h2>
                 <p className="text-xs md:text-lg lg:text-xl mb-4">
                     Unless otherwise stated, Lumijobs and/or its licensors own the intellectual property rights for all material on Lumijobs. All intellectual property rights are reserved. You may access this from Lumijobs for your own personal use subjected to restrictions set in these terms and conditions.
                 </p>
-                <h2 className="text-2xl font-semibold mb-4">You must not:</h2>
+                <h2 className="text:xs md:text-lg lg:text-2xl font-semibold mb-4">You must not:</h2>
                 <ul className="list-disc pl-6 text-xs md:text-lg lg:text-xl mb-4">
                     <li>Republish material from Lumijobs</li>
                     <li>Sell, rent or sub-license material from Lumijobs</li>
@@ -28,7 +28,7 @@ const TermsConditions: React.FC = () => {
                 <p className="text-xs md:text-lg lg:text-xl mb-4">
                     This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the Terms And Conditions Generator and the Privacy Policy Generator.
                 </p>
-                <h2 className="text-2xl font-semibold mb-4">Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website.</h2>
+                <h2 className="text:xs md:text-lg lg:text-2xl font-semibold mb-4">Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website.</h2>
                 <p className="text-xs md:text-lg lg:text-xl mb-4">
                     Lumijobs does not filter, edit, publish, or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Lumijobs, its agents, and/or affiliates. Comments reflect the views and opinions of the person who posts their views and opinions. To the extent permitted by applicable laws, Lumijobs shall not be liable for the Comments or for any liability, damages, or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
                 </p>
