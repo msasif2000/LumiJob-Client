b2e5851768f557fd77e9a8554bd1124c34a4b36b
Foysal79 <onthor312001@gmail.com>
2024-03-05 00:56:39 +0600

    feedback for collect for frontend

--- src/component/websiteFeedback/Feedback.tsx
+++ src/component/websiteFeedback/Feedback.tsx
@@ -54,7 +54,7 @@ const Feedback: React.FC = () => {
         Swal.fire({
           position: "center",
           icon: "success",
-          title: "Your work has been saved",
+          title: "Thank for Your Feedback",
           showConfirmButton: false,
           timer: 1500,
         });
