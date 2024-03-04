7019a283607a2c005b21398947b21674cc2790bf
Asif <msasifiiuc@gmail.com>
2024-03-04 08:29:36 +0600

    no data card ready and Applied jobs data card showing

--- src/component/NoData/NoData.tsx
+++ src/component/NoData/NoData.tsx
@@ -1,3 +1,4 @@
+import noDataImg from '../../assets/image/noData.jpg'
 interface NoDataProps {
     text: string;
     btn: string;
@@ -8,20 +9,23 @@ const NoData: React.FC<NoDataProps> = ({ text, btn, noDataClick }) => {
     return (
         <div className="navbar">
 
-            <div className="md:navbar-start">
+            <div className="md:navbar-start md:hidden">
                 {/* for small device */}
-                <div className="flex gap-5 justify-end">
-                    <a className="text-xl md:text-3xl font-semibold">{text}</a>
+                <div className="flex flex-col items-center gap-3 justify-center">
+                    <img src={noDataImg} alt="" />
+                    <a className="text-xl md:text-3xl font-semibold py-2">{text}!</a>
                     <div className=" flex justify-end gap-2 text-xs mb-5 md:hidden">
-                        
-                        <button className="border-2 bg-slate-100 py-1 px-2 hover:bg-slate-200"><a onClick={noDataClick} className="">{btn}</a></button>
+                        <a onClick={noDataClick} className="btn bg-accent text-white">{btn}</a>
                     </div>
+
                 </div>
             </div>
             {/* for large device  */}
-            <div className="navbar-end hidden md:flex">
+            <div className="xl:navbar-end hidden md:flex flex-col items-center justify-center mx-auto mt-12">
+                <img src={noDataImg} alt="" className=''/>
+                <a className="text-xl md:text-3xl font-semibold py-2">{text}!</a>
                 <ul className="menu menu-horizontal px-1 gap-2">
-                <button className="border-2 bg-slate-100 py-1 px-2 hover:bg-slate-200"><a onClick={noDataClick} className="">{btn}</a></button>
+                    <a onClick={noDataClick} className="btn bg-accent text-white">{btn}</a>
                 </ul>
             </div>
         </div>
