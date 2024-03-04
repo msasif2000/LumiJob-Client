9ad4db0a4003cf1b519f496f1bf3be8ab38bb575
mehedihasanepu <mehedihasanepu123@gmail.com>
2024-03-04 20:43:56 +0600

    Create Team form data get successful

--- src/Pages/CollaborationHub/TaskDetails.tsx
+++ src/Pages/CollaborationHub/TaskDetails.tsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
 import TeamCard from "./TeamCard";
-const TaskDetails = () => {
-    const [openModal, setOpenModal] = useState(false);
+import useAuth from "../../hooks/useAuth";
 
+const TaskDetails = () => {
+    const { user } = useAuth()
+    const [openModal, setOpenModal] = useState(false);
+    const handleSubmitTeamForm = (e: any) => {
+        e.preventDefault();
+        const teamName = e.target.teamName.value;
+        const leaderName = e.target.leaderName.value;
+        const leaderEmail = user.email;
+        const discordLink = e.target.discordLink.value;
+        console.log(teamName, leaderName, leaderEmail, discordLink);
+    }
+    
     return (
         <div className="max-w-screen-2xl mx-auto py-3 px-4">
 
@@ -39,7 +50,7 @@ const TaskDetails = () => {
                         <button onClick={() => setOpenModal(true)} className="bg-gray-700 text-white p-2 rounded-lg">Create</button>
                         <div onClick={() => setOpenModal(false)} className={`fixed flex justify-center items-center z-[100] ${openModal ? 'visible opacity-1' : 'invisible opacity-0'} inset-0 w-full h-full backdrop-blur-sm bg-black/20 duration-100`}>
                             <div onClick={(e_) => e_.stopPropagation()} className={`absolute w-full lg:w-[500px] bg-white drop-shadow-2xl rounded-lg ${openModal ? 'opacity-1 duration-300 translate-y-0' : '-translate-y-20 opacity-0 duration-150'}`}>
-                                <form  className="p-12">
+                                <form onSubmit={handleSubmitTeamForm} className="p-12">
                                     <svg onClick={() => setOpenModal(false)} className="w-10 mx-auto mr-0 cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z" fill="#000000"></path></g></svg>
                                     <h1 className="backdrop-blur-sm text-4xl pb-8">Create Team</h1>
                                     <div className="space-y-5">
