13d2b205e4974942964d93ac68f98667a650faf5
Foysal79 <onthor312001@gmail.com>
2024-03-05 19:39:07 +0600

    marqure card set company

--- src/Pages/FeedbackForWebsiteHome/FeedbackCard.tsx
+++ src/Pages/FeedbackForWebsiteHome/FeedbackCard.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import info from "./info";
 import Aos from "aos";
 import './feedbackCard.css'
+import { FaRegStar, FaStar } from "react-icons/fa";
 interface Data {
   info: info;
 }
@@ -17,27 +18,48 @@ const FeedbackCard: React.FC<Data> = ({ info }) => {
     role,
     PostedDate,
   } = info;
-  const jobPostId = _id;
+  
 
   useEffect(() => {
     Aos.init();
   }, []);
+
+
+  const renderStarRating = (value: number) => {
+    return (
+      <div className="flex">
+        {[...Array(5)].map((_, index) => {
+          const starRating = index + 1;
+          return (
+            <span key={index}>
+              {value >= starRating ? (
+                <FaStar className="text-[#4869DD] cursor-pointer text-xl mx-1" />
+              ) : (
+                <FaRegStar className="text-[#4869DD] cursor-pointer text-xl mx-1" />
+              )}
+            </span>
+          );
+        })}
+      </div>
+    );
+  };
+  
   return (
     <div className="mx-8">
       <div className="card1">
         <div className="header1">
-          <div className="image"></div>
+          
           <div>
             <div className="stars1">
               {/* rating */}
+              {renderStarRating(interfaceRating)}
             </div>
-            <p className="name1">John Doe</p>
+            <p className="name1 grid "> <span>{UserNames}</span></p>
           </div>
         </div>
-
+        
         <p className="message">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-          voluptatem alias ut provident sapiente repellendus.
+          {anyComments}
         </p>
       </div>
     </div>
