dab08353e12220e87e4ecb23200523303922f46a
Rakibul Islam <mdrakib.5627@gmail.com>
2024-03-06 23:03:35 +0600

    candidate pro update

--- src/Company/Update/CompanyProUpdate.tsx
+++ src/Company/Update/CompanyProUpdate.tsx
@@ -46,7 +46,7 @@ const CompanyProUpdate = () => {
 
   useEffect(() => {
     if (user?.email) {
-      axiosPublic.get(`/user-profile/${user.email}`)
+      axiosSecure.get(`/user-profile/${user.email}`)
         .then((res) => {
           setCompany(res.data);
         
